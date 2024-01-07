c6514f8393cafc5854cf6390c24f1e5e1a54facb
FFRfree <free.sun@foxmail.com>
2024-01-07 18:20:00 +0800

    feat(client): add click-to-componet

--- apps/web/app/admin/layout.tsx
+++ apps/web/app/admin/layout.tsx
@@ -4,6 +4,7 @@ import { sidebarItems } from "./sidebar-config";
 import { TopMenu } from "@web/app/admin/top-menu";
 import { ThemeSwitcher } from "./theme-switcher";
 import { Separator } from "@web/components/ui/separator";
+import { ClickToComponent } from "click-to-react-component";
 
 // export const metadata = {
 //   title: "后台管理",
@@ -15,17 +16,21 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className=" h-screen p-2">
-      <div className=" h-full rounded-lg border-2 border-primary">
-        <SideMenu
-          defaultLayout={undefined}
-          navCollapsedSize={2}
-          items={sidebarItems}
-        >
-          <TopMenu />
-          <div className="admin-main-area h-full">{children}</div>
-        </SideMenu>
+    <>
+      <ClickToComponent />
+      <div className=" h-screen p-2">
+        <div className=" h-full rounded-lg border-2 border-primary">
+          <SideMenu
+            defaultLayout={undefined}
+            navCollapsedSize={2}
+            items={sidebarItems}
+          >
+            <TopMenu />
+            <div className="admin-main-area h-full">{children}</div>
+          </SideMenu>
+          ClickToComponent
+        </div>
       </div>
-    </div>
+    </>
   );
 }
